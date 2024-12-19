e68b49b2614b4a32117f45d30be2024e5e5f0de6
Syer20 <samirespinasyer@gmail.com>
2024-12-19 16:54:06 -0400

    corrección para islr en ventas

--- l10n_ve_withholding/models/account_tax.py
+++ l10n_ve_withholding/models/account_tax.py
@@ -17,13 +17,13 @@ class AccountTax(models.Model):
         selection_add=([
             ('partner_tax', 'Alícuota en el Partner'),
         ]), ondelete={'partner_tax': 'set default'}
-    )
-    withholding_type = fields.Selection(
-        selection_add=([
-            ('tabla_islr', 'Tabla ISLR'),
-            ('partner_tax', 'Alícuota en el Partner'),
-        ]), ondelete={'tabla_islr': 'set default', 'partner_tax': 'set default'}
-    )
+    ),
+    # withholding_type = fields.Selection(
+    #     selection_add=([
+    #         ('tabla_islr', 'Tabla ISLR'),
+    #         ('partner_tax', 'Alícuota en el Partner'),
+    #     ]), ondelete={'tabla_islr': 'set default', 'partner_tax': 'set default'}
+    # )
 
     def get_withholding_vals(self, payment_group):
         commercial_partner = payment_group.commercial_partner_id
--- tax_withholdings/models/account_tax.py
+++ tax_withholdings/models/account_tax.py
@@ -12,12 +12,22 @@ class AccountTax(models.Model):
     _inherit = "account.tax"
 
     withholding_type = fields.Selection(
-        selection=[
+        selection_add=([
             ("iva", "VAT withholding"),
-            ("none", "No withholding"),  # Agrega esta línea
-        ],
+            ('tabla_islr', 'Tabla ISLR'),
+            ('partner_tax', 'Alícuota en el Partner'),
+            ("none", "No withholding"),
+        ]),
+        ondelete={
+            'iva': 'set default',
+            'tabla_islr': 'set default',
+            'partner_tax': 'set default',
+            'none': 'set default',
+        },
+        default="none",  # Define un valor predeterminado para el campo
         string="Type retention"
     )
+
 
     @api.model
     def _prepare_tax_totals(self, base_lines, currency, tax_lines=None, **kwargs):
