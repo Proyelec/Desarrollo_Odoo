bc6369b8a70a2dbc5bef3034aa38cac37452dc7d
Syer20 <76893177+Syer20@users.noreply.github.com>
2025-06-17 05:21:15 -0400

    Referencia Interna única

---
+++ product_default_code_unique_required/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Unique and Required Default Code",
+    "version": "17.0.1.0.0",
+    "category": "Inventory",
+    "depends": ["product"],
+    "author": "Samir Espina by Contables",
+    "license": "LGPL-3",
+    "description": "Hace que el código interno (default_code) sea obligatorio y único.",
+    "installable": True,
+    "application": False,
+    "auto_install": False
+}
