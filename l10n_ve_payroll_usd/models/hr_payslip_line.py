4bcda19ed7d5343e6e5a2e13220cebb78527ad9b
Syer20 <76893177+Syer20@users.noreply.github.com>
2025-06-20 15:39:03 -0400

    Corrección de Días en Recibos

--- l10n_ve_payroll_usd/models/hr_payslip_line.py
+++ l10n_ve_payroll_usd/models/hr_payslip_line.py
@@ -31,27 +31,123 @@ class HRPayslipLine(models.Model):
                 record.total_ref = 0
             record.total = record.amount
 
-    @api.depends('name', 'total_ref', 'salary_rule_id')
+    @api.depends('name', 'total_ref', 'salary_rule_id', 'slip_id.contract_id.anios_antiguedad')
     def _compute_dias(self):
-        valor_dias = ""
-        valor_horas = ""
         for rec in self:
+            valor_dias = ""
+            valor_horas = ""
             worked_days_line_ids = rec.slip_id.worked_days_line_ids
-            if rec.category_id.code == 'BASIC':
-                valor_dias = worked_days_line_ids.filtered(lambda x: x.code == 'WORK100').number_of_days + worked_days_line_ids.filtered(lambda x: x.code == 'AUSEP').number_of_days
-            else:
-                worked_days_line_ids = rec.slip_id.worked_days_line_ids.filtered(lambda x: x.code == rec.code)
-                if rec.salary_rule_id.mostrar_cantidad == 'dias':
-                    if len(worked_days_line_ids) > 0:
-                        valor_dias = round(worked_days_line_ids.number_of_days, 2)
+            
+            # Cálculo especial para ADIVACA y ADIBONOVACA
+            if rec.code == 'ADIVACA':
+                if rec.slip_id.contract_id.anios_antiguedad <= 1:
+                    valor_dias = rec.slip_id.contract_id.anios_antiguedad - 1
+                else:
+                    valor_dias = rec.slip_id.contract_id.anios_antiguedad
+            elif rec.code == 'GPSAB':
+                prestaciones = self.env['hr.employee.prestaciones'].search(
+                    [('employee_id', '=', rec.slip_id.employee_id.id)],
+                    order='create_date desc',
+                    limit=1
+                )
+                valor_dias = prestaciones.dias_acumulados if prestaciones else 0
+            elif rec.code == 'GPSC':
+                prestaciones = self.env['hr.employee.prestaciones'].search(
+                    [('employee_id', '=', rec.slip_id.employee_id.id)],
+                    order='create_date desc',
+                    limit=1
+                )
+                valor_dias = prestaciones.dias_adici_acumulado if prestaciones else 0
+            elif rec.code == 'VACAV':
+                if rec.contract_id.anios_antiguedad > 0:
+                    valor_dias = rec.slip_id.contract_id.anios_antiguedad + 15
+                else:
+                    valor_dias = 0
+            elif rec.code == 'BONOVACAV':
+                if rec.contract_id.anios_antiguedad > 0:
+                    valor_dias = rec.slip_id.contract_id.anios_antiguedad + 16
+                else:
+                    valor_dias = 0
+            elif rec.code == 'VACAF':
+                if rec.contract_id.anios_antiguedad > 0:
+                    valor_dias = rec.slip_id.x_studio_dias_de_vacaciones_no_disfrutados + 15
+                else:
+                    hoy = rec.slip_id.date_to
+                    fecha_ingreso = rec.contract_id.date_start
+
+                    if hoy and fecha_ingreso:
+                        anios_antiguedad = rec.contract_id.anios_antiguedad
+
+                        if anios_antiguedad > 0:
+                            mes_actual = hoy.month
+                        else:
+                            mes_actual = hoy.month - fecha_ingreso.month
+                            if mes_actual < 0:
+                                mes_actual = 0  # Evitar meses negativos si está en el mismo año y mes de ingreso futuro
+
+                        valor_dias = ((15 + anios_antiguedad) / 12) * mes_actual
+            elif rec.code == 'BONOVACAF':
+                if rec.contract_id.anios_antiguedad > 0:
+                    valor_dias = rec.slip_id.x_studio_bono_vacacional_no_disfrutado + 16
+                else:
+                    hoy = rec.slip_id.date_to
+                    fecha_ingreso = rec.contract_id.date_start
+
+                    if hoy and fecha_ingreso:
+                        anios_antiguedad = rec.contract_id.anios_antiguedad
+
+                        if anios_antiguedad > 0:
+                            mes_actual = hoy.month
+                        else:
+                            mes_actual = hoy.month - fecha_ingreso.month
+                            if mes_actual < 0:
+                                mes_actual = 0  # Evitar meses negativos si está en el mismo año y mes de ingreso futuro
+
+                        valor_dias = ((16 + anios_antiguedad) / 12) * mes_actual
+            elif rec.code == 'UTILIDF':
+                hoy = rec.slip_id.date_to
+                fecha_ingreso = rec.contract_id.date_start
+
+                if hoy and fecha_ingreso:
+                    anios_antiguedad = rec.contract_id.anios_antiguedad
+
+                    if anios_antiguedad > 0:
+                        mes_actual = hoy.month
                     else:
-                        valor_dias = ""
+                        mes_actual = hoy.month - fecha_ingreso.month
+                        if mes_actual < 0:
+                            mes_actual = 0  # Evitar meses negativos si está en el mismo año y mes de ingreso futuro
+
+                    valor_dias = (60 / 12) * mes_actual
+                else:
+                    valor_dias = 0
+            elif rec.code == 'ADIBONOVACA':
+                    valor_dias = rec.slip_id.contract_id.anios_antiguedad
+            elif rec.code == 'BONOVACA':
+                valor_dias = rec.slip_id.contract_id.dias_bono_vacacional
+            elif rec.code == 'VACA':
+                valor_dias = 15
+            elif rec.code == 'DDFVACA':
+                valor_dias = (
+                    worked_days_line_ids.filtered(lambda x: x.code == 'DDFVACA').number_of_days
+                )
+            elif rec.code == 'DPSUELDO':
+                valor_dias = (
+                    worked_days_line_ids.filtered(lambda x: x.code == 'DDESS').number_of_days +
+                    worked_days_line_ids.filtered(lambda x: x.code == 'DDESD').number_of_days
+                )
+            elif rec.category_id.code == 'BASIC':
+                valor_dias = (
+                    worked_days_line_ids.filtered(lambda x: x.code == 'WORK100').number_of_days +
+                    worked_days_line_ids.filtered(lambda x: x.code == 'AUSEP').number_of_days
+                )
+            else:
+                worked_days_line_ids = worked_days_line_ids.filtered(lambda x: x.code == rec.code)
+                if rec.salary_rule_id.mostrar_cantidad == 'dias':
+                    valor_dias = round(worked_days_line_ids.number_of_days, 2) if worked_days_line_ids else ""
                     valor_horas = ""
                 elif rec.salary_rule_id.mostrar_cantidad == 'horas':
-                    if len(worked_days_line_ids) > 0:
-                        valor_horas = round(worked_days_line_ids.number_of_hours, 2)
-                    else:
-                        valor_horas = ""
+                    valor_horas = round(worked_days_line_ids.number_of_hours, 2) if worked_days_line_ids else ""
                     valor_dias = ""
                 else:
                     valor_dias = ""
