994cad9b8b8cfcdff5b55ba0e16042dc543528e5
jdvillasmil <jdvillasmil007@gmail.com>
2026-05-15 15:41:35 -0400

    fix(proyelec_sale_createdby): corregir autor en manifest

--- proyelec_sale_createdby/__manifest__.py
+++ proyelec_sale_createdby/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Proyelec - Creado Por en Presupuestos',
     'version': '17.0.1.0.0',
-    'author': 'Contables Boyer',
+    'author': 'Juan David Villasmil - AIT Proyelec',
     'category': 'Sales',
     'summary': 'Agrega columna Creado Por en la vista lista de Presupuestos',
     'depends': ['sale'],
