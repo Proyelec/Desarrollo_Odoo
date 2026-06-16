19cfdf61d88d522e80caf56965ffbe84fd7eea5d
Proyelec <ait@proyelec.org>
2026-06-16 16:47:54 -0400

    feat: agregar vista gráfico KPI conversión de líneas (Ganado vs Pendiente) con filtros temporales

--- proyelec_so_to_po/__manifest__.py
+++ proyelec_so_to_po/__manifest__.py
@@ -7,6 +7,7 @@
     "data": [
         "security/ir.model.access.csv",
         "views/sale_order_views.xml",
+        "views/kpi_conversion_views.xml",
     ],
     "installable": True,
     "application": False,
